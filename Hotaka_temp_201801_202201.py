da83ed4dd52004391c35944ad6b62cc92bd75c9b
azumi <adumi29deviser@gmail.com>
2022-12-12 15:16:45 +0900

    complete

---
+++ Hotaka_temp_201801_202201.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+ 
+#CSVファイルをANSI形式で読み込む
+data = pd.read_csv('Hotaka_temp_201801_202201.csv',encoding = 'ANSI',skiprows=[0])
+print(data)
+
+# グラフに出力
+data.plot()
+plt.title('2018年～2022年2月の穂高の気温')
+plt.xlabel('日付(日)')
+plt.ylabel('気温(℃)')
+
+# グラフをpngに保存
+plt.savefig("hotaka_temp.png")
+
+plt.show()
